15e1103045ce97b1f2ee71ea0e58443888b4fcd8
vrushab-moolya <vrushab.patil@moolya.com>
2020-12-12 13:34:25 +0530

    product view

--- demo.py
+++ demo.py
@@ -15,19 +15,7 @@ time.sleep(2)
 cart=driver.find_element(By.XPATH,'//button[@class="btn_primary btn_inventory"]')
 cart.click()
 
-driver.find_element(By.CSS_SELECTOR,'[fill="currentColor"]').click()
-product=driver.find_element(By.XPATH,"//div[@class='inventory_item_name']")
-if product.text== 'Sauce Labs Backpack' :
-    print('product is verified')
 
-driver.find_element(By.XPATH,"//a[.='CHECKOUT']").click()
-
-driver.find_element(By.ID,'first-name').send_keys('vrushab')
-driver.find_element(By.ID,'last-name').send_keys('patil')
-driver.find_element(By.ID,'postal-code').send_keys('590006')
-driver.find_element(By.XPATH,"//input[@type='submit']").click()
-
-driver.find_element(By.XPATH,'//a[@class="btn_action cart_button"]').click()
 
 
 
